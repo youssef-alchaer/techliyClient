b93e7305743dfc6a286c3b8307c52a532f98f82f
Youssef Alchaer <alchaeyo@sheridancollege.ca>
2023-01-01 12:23:24 -0500

    Added more advanced functions, got machine name and more parms from OS

---
+++ techliyClient/Functions/OperatingSystemInfo.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace techliyClient.Functions
+{
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+
+    public class OperatingSystemInfo
+    {
+        public string Caption { get; private set; }
+
+        public string OperatingSystemArchitecture { get; private set; }
+
+        public string OperatingSystemServicePack { get; private set; }
+
+        public string ProcessorName { get; private set; }
+
+
+        public OperatingSystemInfo()
+        {
+            getOperatingSystemInfo();
+            getProcessorInfo();
+
+        }
+
+        public void getOperatingSystemInfo()
+        {
+            Console.WriteLine("Displaying operating system info....\n");
+            //Create an object of ManagementObjectSearcher class and pass query as parameter.
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_OperatingSystem");
+            foreach (ManagementObject managementObject in mos.Get())
+            {
+                if (managementObject["Caption"] != null)
+                {
+                    //  OperatingSystemArchitecture =  managementObject["Caption"].ToString();   //Display operating system caption
+
+                    Caption = managementObject["Caption"].ToString() ?? "Unknown";
+                }
+                if (managementObject["OSArchitecture"] != null)
+                {
+                //    Console.WriteLine("Operating System Architecture  :  " + managementObject["OSArchitecture"].ToString());
+
+                    OperatingSystemArchitecture = managementObject["OSArchitecture"].ToString() ?? "Unknown";
+                }
+                if (managementObject["CSDVersion"] != null)
+                {
+                    OperatingSystemServicePack = managementObject["CSDVersion"].ToString() ?? "Unknown";
+
+                }
+            }
+        }
+
+            public void getProcessorInfo()
+        {
+            Console.WriteLine("\n\nDisplaying Processor Name....");
+            RegistryKey processor_name = Registry.LocalMachine.OpenSubKey(@"Hardware\Description\System\CentralProcessor\0", RegistryKeyPermissionCheck.ReadSubTree);   //This registry entry contains entry for processor info.
+
+            if (processor_name != null)
+            {
+                if (processor_name.GetValue("ProcessorNameString") != null)
+                {
+                 //   Console.WriteLine(processor_name.GetValue("ProcessorNameString"));   //Display processor ingo.
+
+                    ProcessorName = processor_name.GetValue("ProcessorNameString").ToString();
+
+                }
+            }
+        }
+
+    }
+}
--- techliyClient/Program.cs
+++ techliyClient/Program.cs
@@ -7,10 +7,27 @@ using System.Runtime.InteropServices;
 using System.Management;
 using techliyClient;
 using System.Diagnostics;
+using techliyClient.Functions;
+using System.Drawing;
+
+
+//https://github.com/clowd/Clowd.Squirrel
 
 Console.WriteLine("Techliy Client Started");
 
 fireStoreFunctions cloud = new fireStoreFunctions();
+
+var os = new OperatingSystemInfo();
+
+var info = new Dictionary<string, string>() { 
+    {nameof(os.Caption), os.Caption }, 
+    {nameof(os.ProcessorName), os.ProcessorName }, 
+    {nameof(os.OperatingSystemArchitecture) , os.OperatingSystemArchitecture } 
+};
+
+cloud.sendData(info, "Clients");
+
+Console.WriteLine();
 
 #region Background tasks
 async Task RunInBackground(TimeSpan timeSpan, Action action)
@@ -24,10 +41,23 @@ async Task RunInBackground(TimeSpan timeSpan, Action action)
 #endregion
 
 
-RunInBackground(TimeSpan.FromSeconds(2), () => getRam(cloud));
+Console.WriteLine();
+
+ /*await RunInBackground(TimeSpan.FromSeconds(2), 
+     action: 
+     () => cloud.sendData(new Dictionary<string, object> 
+     {
+         {"Ram Useage", ClientFunctions.getRam().ToString() } 
+     },
+     "Clients", 
+     "AMD Desktop")
+     );*/
+
+Console.Read();
 
 
-cloud.sendData(new Dictionary<string, object> { { "Ram Useage", percent.ToString() } }, "Clients", "AMD Desktop");
+
+//cloud.sendData(new Dictionary<string, object> { { "Ram Useage", percent.ToString() } }, "Clients", "AMD Desktop");
 
 
 
